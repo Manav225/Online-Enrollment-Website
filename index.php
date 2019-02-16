ee67479e89c3ab453e1c575524f7612d73c98934
KeijiMizumura <keijimizumura09@gmail.com>
2019-02-15 22:01:18 -0800

    Added header and navigation

--- index.php
+++ index.php
@@ -9,11 +9,17 @@
     <link rel="stylesheet" href="styles/main.css">
     <link rel="stylesheet" href="styles/footer.css">
     <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
 </head>
 <body>
     
+    <?php
+        include_once "layouts/header.php";
+    ?>
 
-
+    <div id="temp">
+        <div class="container"></div>
+    </div>
 
     <?php
         include_once "layouts/footer.php";
---
+++ layouts/header.php
@@ -0,0 +1,36 @@
+<header>
+        <div id="top_header">
+            <div class="container">
+                <p>Online Portal for Taguig Integrated School</p>
+                <div class="top_nav">
+                    <a href="#">Admin Dashboard</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Registration</a>
+                </div>
+            </div>
+        </div>
+        <div id="mid_header">
+            <div class="container">
+                <div class="inner">
+                    <div class="logo">
+                        <img src="./images/logo.png" alt="Taguig Integrated School">
+                    </div>
+                    <div class="title">
+                        <h1>Taguig Integrated School</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="bottom_header">
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Events</a></li>
+                        <li><a href="#">Teachers</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
