f818be47dfc7157cf8d30e552a67fd944f267b70
KeijiMizumura <keijimizumura09@gmail.com>
2019-02-15 21:16:40 -0800

    Added footer

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>School - Home</title>
+    <title>Taguig Integrated School - Home</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/footer.css">
+    <!-- Fonts -->
 </head>
 <body>
     
+
+
+
+    <?php
+        include_once "layouts/footer.php";
+    ?>
+
 </body>
 </html>
---
+++ layouts/footer.php
@@ -0,0 +1,56 @@
+<footer id="footer"> 
+      <div class="bg"></div>  
+      <div class="content"> 
+        <div class="group"> 
+          <div class="col span_1_of_3"> 
+            <h2>Links</h2>  
+            <div class="vmenu"> 
+              <ul> 
+                <li>
+                  <a href="#">Lorem ipsum</a>
+                </li>  
+                <li>
+                  <a href="#">Vivamus</a>
+                </li>  
+                <li>
+                  <a href="#">Suspendisse</a>
+                </li> 
+              </ul> 
+            </div> 
+          </div>  
+          <div class="col span_1_of_3"> 
+            <h2>About Us</h2>  
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec.</p> 
+          </div>  
+          <div class="col span_1_of_3"> 
+            <h2>Contact</h2>  
+            <p>Your Company Ltd
+              <br /> 10 Somewhere Street, WWW Design City
+              <br /> 
+            </p> 
+          </div> 
+        </div>  
+        <div class="clear"></div>  
+        <div class="baseline"> 
+          <div style="float:left;margin-top:7px"> 
+            <p> 
+              <!-- Update your organization copyright --> Copyright - 2019 - Your Organization | All Rights Reserved | 
+              <!-- This template is free. In respect with copyright, please keep this credit link visible and unmodified (except for translation purposes) --> Designed with doTemplate. Create your custom 
+              <a href="http://www.dotemplate.com">website template</a> easily.
+            </p> 
+          </div>  
+          <ul class="social-links" style="float:right"> 
+            <li> 
+              <a href="#" rel="nofollow" style="background-image:url('./images/facebook.png')" title="Be a fan on Facebook"></a> 
+            </li>  
+            <li> 
+              <a href="#" rel="nofollow" style="background-image:url('./images/twitter.png')" title="Follow us on Twitter"></a> 
+            </li>  
+            <li> 
+              <a href="#" style="background-image:url('./images/googleplus.png')" title="Circle us on Google+"></a> 
+            </li>  
+          </ul>  
+          <div class="clear"></div> 
+        </div> 
+      </div> 
+</footer> 
